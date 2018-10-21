6b4afff96c976de0c6fe7d152cc3e33027b93772
Joshua Chen <joshuachen1@cpp.edu>
2018-10-20 18:05:56 -0700

    add generateHuffmanTree method

--- HuffmanConstitution.java
+++ HuffmanConstitution.java
@@ -15,15 +15,48 @@ public class HuffmanConstitution {
 
         String constitution = new String(Files.readAllBytes(Paths.get("/Users/student/IdeaProjects/Huffman/src/Constitution.txt")));
 
+        // Minheap of FreqNode
+        PriorityQueue<FreqNode> huffTree = new PriorityQueue<>();
+
+        long timeIn = System.nanoTime();
+        generateHuffmanTree(huffTree, constitution);
+        long timeOut = System.nanoTime() - timeIn;
+
+        System.out.println("Single Thread Creation of Tree Time: " + timeOut * 1e-6 + " (ms).");
+
+        HashMap<Character, String> compressedChar = new HashMap<>();
+
+        timeIn = System.nanoTime();
+
+        FileOutputStream compressedConstitution = new FileOutputStream("/Users/student/IdeaProjects/Huffman/src/CompressedConstitution.txt");
+
+        generateCompressedChar(compressedChar, new StringBuffer(), huffTree.peek());
+
+        StringBuffer encodeConst = new StringBuffer();
+
+        for (int i = 0; i < constitution.length(); i++) {
+            encodeConst.append(compressedChar.get(constitution.charAt(i)));
+        }
+
+        compressedConstitution.write(encodeConst.toString().getBytes());
+        compressedConstitution.close();
+
+        timeOut = System.nanoTime() - timeIn;
+
+        System.out.println("Single Thread Encoding of file Time: " + timeOut * 1e-6 + " (ms).");
+
+        System.out.println("Original Size contains " + constitution.length() + " bytes.");
+        System.out.println("Compressed Size contains " + encodeConst.toString().length() / 8.0 + " bytes.");
+        System.out.printf("Original Compressed by %.2f%%.", 100 -(encodeConst.toString().length() / 8.0) / constitution.length() * 100);
+    }
+
+    public static void generateHuffmanTree(PriorityQueue<FreqNode> huffTree, String constitution) {
         // Create a frequency array of each char's ascii value used for the index of the array
         // and increment the value of that index by 1 when the char is read
         int[] charFreq = new int[256];
         for (char c : constitution.toCharArray()) {
             charFreq[c] += 1;
         }
-
-        // Minheap of FreqNode
-        PriorityQueue<FreqNode> huffTree = new PriorityQueue<>();
 
         // Make each letter its own mini tree sorted with the head of the minheap as the lowest frequency
         for (int i = 0; i < charFreq.length; i++) {
@@ -44,24 +77,6 @@ public class HuffmanConstitution {
             assert fNode2 != null;
             huffTree.offer(new FreqNode(fNode1, fNode2));
         }
-
-        HashMap<Character, String> compressedChar = new HashMap<>();
-
-        generateCompressedChar(compressedChar, new StringBuffer(), huffTree.peek());
-
-        FileOutputStream compressedConstitution = new FileOutputStream("/Users/student/IdeaProjects/Huffman/src/CompressedConstitution.txt");
-
-        StringBuffer encodeConst = new StringBuffer();
-        for (int i = 0; i < constitution.length(); i++) {
-            encodeConst.append(compressedChar.get(constitution.charAt(i)));
-        }
-
-        compressedConstitution.write(encodeConst.toString().getBytes());
-        compressedConstitution.close();
-
-        System.out.println("Original Size contains " + constitution.length() + " bytes.");
-        System.out.println("Compressed Size contains " + encodeConst.toString().length() / 8.0 + " bytes.");
-        System.out.printf("Original Compressed by %.2f%%.", 100 -(encodeConst.toString().length() / 8.0) / constitution.length() * 100);
     }
 
     // Recursively determine the compressed binary string of each character in the tree
@@ -71,12 +86,14 @@ public class HuffmanConstitution {
             hm.put(((CharNode) currentNode).character, compressedStr.toString());
 
             // Display the Character, Frequency in Original text, and new Binary String to encode the character
+            /*
             if (((CharNode) currentNode).character == '\n') {
                 System.out.printf("%5s \t %5d \t %15s\n", "\\n", ((CharNode) currentNode).freq, compressedStr.toString());
             }
             else {
                 System.out.printf("%5c \t %5d \t %15s\n", ((CharNode) currentNode).character, ((CharNode) currentNode).freq, compressedStr.toString());
             }
+            */
         }
         else if (currentNode != null) {
             // Traverse left of currentNode
