d87fa43daebdfafff138cf8e114c967bb606584c
Joshua Chen <joshuachen1@cpp.edu>
2018-10-20 21:20:09 -0700

    Slightly faster Huffman Tree

--- ParallelHuffmanTree.java
+++ ParallelHuffmanTree.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.PriorityQueue;
-import java.util.concurrent.Phaser;
 
 /**
  * @author Joshua Chen
@@ -10,22 +9,18 @@ import java.util.concurrent.Phaser;
  * Date: Oct 22, 2018
  */
 public class ParallelHuffmanTree {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         String constitution = new String(Files.readAllBytes(Paths.get("/Users/student/IdeaProjects/Huffman/src/Constitution.txt")));
 
         // Minheap of FreqNode
         PriorityQueue<FreqNode> huffTree = new PriorityQueue<>();
 
-        long timeIn = System.nanoTime();
         generateHuffmanTree(huffTree, constitution);
-        long timeOut = System.nanoTime() - timeIn;
-
-        System.out.println("Parallel Thread Creation of Tree Time: " + timeOut * 1e-6 + " (ms).");
     }
 
-    public static void generateHuffmanTree(PriorityQueue<FreqNode> huffTree, String constitution) {
-        Phaser p = new Phaser();
+    public static void generateHuffmanTree(PriorityQueue<FreqNode> huffTree, String constitution) throws InterruptedException {
+        long timeIn = System.nanoTime();
 
         // Create a frequency array of each char's ascii value used for the index of the array
         // and increment the value of that index by 1 when the char is read
@@ -41,6 +36,34 @@ public class ParallelHuffmanTree {
             }
         }
 
+        Thread t0 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < charFreq.length / 2; i++) {
+                    if (charFreq[i] > 0) {
+                        huffTree.offer(new CharNode(charFreq[i], (char) i));
+                    }
+                }
+            }
+        });
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = charFreq.length / 2; i < charFreq.length; i++) {
+                    if (charFreq[i] > 0) {
+                        huffTree.offer(new CharNode(charFreq[i], (char) i));
+                    }
+                }
+            }
+        });
+
+        t0.start();
+        t1.start();
+
+        t0.join();
+        t1.join();
+
         // Combining the mini trees in the minheap until only one tree containing everything remains
         while (huffTree.size() > 1) {
             // Least frequent letter in minheap
@@ -52,5 +75,8 @@ public class ParallelHuffmanTree {
             assert fNode2 != null;
             huffTree.offer(new FreqNode(fNode1, fNode2));
         }
+        long timeOut = System.nanoTime() - timeIn;
+
+        System.out.println("Parallel Thread Creation of Tree Time: " + timeOut * 1e-6 + " (ms).");
     }
 }
